614979f1603a400f2b0470b55af3c67107a2966c
Chasbrot <m.selinger@outlook.at>
2022-12-27 22:00:58 +0100

    fix/error message
    Fixed so that error messages are reliable and don't crash the app

--- dist/public/vue/helper.js
+++ dist/public/vue/helper.js
@@ -37,40 +37,51 @@ export default {
     getRandColor() {
         var col = [0, 0, 0]; // RGB Color values
         // ?
-        var ff = Math.floor(Math.random()*3); 
-        var oo = Math.floor(Math.random()*3);
+        var ff = Math.floor(Math.random() * 3);
+        var oo = Math.floor(Math.random() * 3);
         var color = Math.floor(Math.random() * 6) * 51;
         // magic? idk
-        col[ff]=255;
-        if(ff==oo){
-            col[++oo%3]=0;
-            oo= ++oo%3;
-        }else{
-            col[oo]=0
+        col[ff] = 255;
+        if (ff == oo) {
+            col[++oo % 3] = 0;
+            oo = ++oo % 3;
+        } else {
+            col[oo] = 0
         }
-        var t = [0,1,2];
-        t.forEach((x)=>{
-            if(x!= oo && x!= ff){
-                col[x]=color;
+        var t = [0, 1, 2];
+        t.forEach((x) => {
+            if (x != oo && x != ff) {
+                col[x] = color;
             }
         });
         // Generate hex string
         var hex = "#";
-        col.forEach((x)=>{
-            if(x==0){
-                hex+="00";
-            }else{
+        col.forEach((x) => {
+            if (x == 0) {
+                hex += "00";
+            } else {
                 hex += x.toString(16);
             }
-           
+
         })
         // Error if not valid hex format
-        if(hex.length != 7){
-            console.log(ff + " "+ oo + " "+ color);
+        if (hex.length != 7) {
+            console.log(ff + " " + oo + " " + color);
             console.log(col);
         }
         return hex;
-    }
+    },
+
+    /*
+    * Show error modal with relevant informations
+    */
+    async makeError(message, request, error) {
+        document.getElementById("errorMessage").innerHTML = message;
+        document.getElementById("errorSource").innerHTML = request;
+        document.getElementById("errorReason").innerHTML = error;
+        var errorModal = new bootstrap.Modal(document.getElementById('errorModal'))
+        errorModal.show()
+    },
 
 
 
--- dist/public/vue/rest.js
+++ dist/public/vue/rest.js
@@ -1,30 +1,14 @@
+import hlp from "/vue/helper.js";
+
 export default {
-
-    /*
-     * Tests if file is loaded correctly and framework is working
-     */
-    async testFunction(string) {
-        return "Hello Test";
-    },
-
-    /*
-    * Show error modal with relevant informations
-    */
-    async makeError(message, request, error) {
-        this.errorModalMessage = message
-        this.errorModalException = request
-        this.errorModalRequest = error
-        var errorModal = new bootstrap.Modal(document.getElementById('errorModal'))
-        errorModal.show()
-    },
-
+    
     /*
      *  Fetch data from a defined url. If error returns null and displays a error
      */
     async fetchData(url) {
         var result = await fetch(url);
         if (result.status != 200) {
-            this.makeError("Anfrage auf den Server fehlgeschlagen.", "GET " + url, result
+            hlp.makeError("Anfrage auf den Server fehlgeschlagen.", "GET " + url, result
                 .statusText);
             console.log(result)
             return null;
@@ -43,7 +27,7 @@ export default {
         const response = await fetch(url,
             requestOptions);
         if (response.status != 200) {
-            this.makeError("Anfrage fehlerhaft.", "PUT " + url + data, response.statusText);
+            hlp.makeError("Anfrage fehlerhaft.", "PUT " + url + data, response.statusText);
             return false;
         }
         // Catch non json answer
@@ -65,7 +49,7 @@ export default {
         const response = await fetch(url,
             requestOptions);
         if (response.status != 200) {
-            this.makeError("Anfrage fehlerhaft.", "POST " + url + data, response.statusText);
+            hlp.makeError("Anfrage fehlerhaft.", "POST " + url + data, response.statusText);
             return false;
         }
         // Catch non json answer
@@ -87,7 +71,7 @@ export default {
         const response = await fetch(url,
             requestOptions);
         if (response.status != 200) {
-            this.makeError("Anfrage fehlerhaft.", "DELETE " + url + data, response.statusText);
+            hlp.makeError("Anfrage fehlerhaft.", "DELETE " + url + data, response.statusText);
             return false;
         }
         return true;
